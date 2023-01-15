d6dcb865991c8e743afb117ea16323114ce596a2
t0ta <olena.a.hrynyuk@gmail.com>
2023-01-15 21:30:20 +0200

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <footer>
+        This App was coded by t0ta, open-sourced on{" "}
+        <a href="https://github.com/t0ta/weather-react-app-new">GitHub</a> and
+        hosted on{" "}
+        <a href="https://lively-rabanadas-1531e6.netlify.app/">Netlify</a>
+      </footer>
     </div>
   );
 }
