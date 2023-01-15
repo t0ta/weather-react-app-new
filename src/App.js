b32b3a86f8dfabc30db2cfa8c753b5bfa08a7247
t0ta <olena.a.hrynyuk@gmail.com>
2023-01-15 21:43:02 +0200

    chore: add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This App was coded by t0ta, open-sourced on{" "}
-        <a href="https://github.com/t0ta/weather-react-app-new">GitHub</a> and
-        hosted on{" "}
-        <a href="https://lively-rabanadas-1531e6.netlify.app/">Netlify</a>
-      </footer>
+      <div className="container">
+        <div className="btn btn-primary">hello</div>
+        <footer>
+          This App was coded by t0ta, open-sourced on{" "}
+          <a href="https://github.com/t0ta/weather-react-app-new">GitHub</a> and
+          hosted on{" "}
+          <a href="https://lively-rabanadas-1531e6.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
